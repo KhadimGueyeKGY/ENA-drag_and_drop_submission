c6f7d76e591e5cd34501d280d2e75826e5119504
Khadim Gueye <89599484+KhadimGueyeKGY@users.noreply.github.com>
2023-01-25 19:51:40 +0000

    Update DragAndDropSubmission.py

--- modules/DragAndDropSubmission.py
+++ modules/DragAndDropSubmission.py
@@ -10,6 +10,7 @@ import getpass
 import os
 from modules.SQLrequest import dbconnection
 import pandas as pd 
+from datetime import datetime
 
 class DDSubmission:
     def submission():
@@ -46,15 +47,16 @@ class DDSubmission:
             
         action = ''
         while action=='':
-            action = input ('$ Specify the type of action needed ( ADD or MODIFY) > ')
+            action = input ('$ Specify the type of action needed ( ADD ) > ')
             if action.upper() == 'ADD':
                 action = 'ADD'
-            elif action.upper()=='MODIFY': 
-                action = 'MODIFY'
+            # elif action.upper()=='MODIFY': 
+            #     action = 'MODIFY'
             else :
                 action = ''
         print('\n\n')
         # submission of Study and Samples
+        start = datetime.now()
         print ('\033[92m'+'\t\t\t+---------------------------------------------------------------+')
         print ('\033[92m'+'\t\t\t|                                                               |')
         print ('\033[92m'+'\t\t\t|========>  Hello '+user.split('$')[0].upper()+' !!!   <========|')
@@ -123,29 +125,41 @@ class DDSubmission:
             for j in range(header_2.index('experiment_name'),header_2.index('uploaded file 2')+1):
                 if header_2[j]== 'experiment_name' :
                     manifest.write('NAME\t'+str(sup_metadata[j])+'\n')
-                elif header_2[j]== 'uploaded file 1' and sup_metadata[j] != '':
-                    manifest.write('FASTQ\t'+str(sup_metadata[j])+'\n')
-                elif header_2[j]== 'uploaded file 2' and sup_metadata[j] != '' :
-                    manifest.write('FASTQ\t'+str(sup_metadata[j])+'\n')
-                elif header_2[j].upper()== 'SEQUENCING_PLATFORM' :
+                elif header_2[j]== 'uploaded file 1':
+                    fq1 = 1
+                    if sup_metadata[j] != -1 :
+                        manifest.write('FASTQ\t'+str(sup_metadata[j])+'\n')
+                    else:
+                        fq1 = 0
+                elif header_2[j]== 'uploaded file 2': 
+                    fq2 = 1
+                    if sup_metadata[j] != -1 :
+                        manifest.write('FASTQ\t'+str(sup_metadata[j])+'\n')
+                    else :
+                        fq2 = 0 
+                elif header_2[j].upper()== 'SEQUENCING_PLATFORM' and sup_metadata[j] != -1  :
                     manifest.write('PLATFORM\t'+str(sup_metadata[j])+'\n')
-                elif header_2[j].upper()== 'SEQUENCING_INSTRUMENT' :
+                elif header_2[j].upper()== 'SEQUENCING_INSTRUMENT' and sup_metadata[j] != -1  :
                     manifest.write('INSTRUMENT\t'+str(sup_metadata[j])+'\n')
-                elif header_2[j].upper()== 'LIBRARY_DESCRIPTION' :
+                elif header_2[j].upper()== 'LIBRARY_DESCRIPTION' and sup_metadata[j] != -1  :
                     manifest.write('DESCRIPTION\t'+str(sup_metadata[j])+'\n')
-                else :
+                elif sup_metadata[j] != -1 :
                     manifest.write(header_2[j].upper()+'\t'+str(sup_metadata[j])+'\n')
             manifest.close()
             
-            if t == '-t':
-                os.system('java -jar modules/webin-cli-5.2.0.jar -context reads -manifest '+directory+'/manifest_run'+str(i-4)+'.txt -inputDir '+directory+' -userName '+webin_accout+' -password '+suppasswd+' -test -validate')
-                print('\n')
-                os.system('java -jar modules/webin-cli-5.2.0.jar -context reads -manifest '+directory+'/manifest_run'+str(i-4)+'.txt -inputDir '+directory+' -userName '+webin_accout+' -password '+suppasswd+' -test -submit')
+            if fq1 != 0 and fq2 != 0 :
+                if t == '-t':
+                    os.system("java -jar modules/webin-cli-5.2.0.jar -context reads -manifest "+directory+"/manifest_run"+str(i-4)+".txt -inputDir "+directory+" -userName "+webin_accout+" -password '"+suppasswd+"' -test -validate")
+                    print('\n')
+                    os.system("java -jar modules/webin-cli-5.2.0.jar -context reads -manifest "+directory+"/manifest_run"+str(i-4)+".txt -inputDir "+directory+" -userName "+webin_accout+" -password '"+suppasswd+"' -test -submit")
+                else :
+                    os.system("java -jar modules/webin-cli-5.2.0.jar -context reads -manifest "+directory+"/manifest_run"+str(i-4)+".txt -inputDir "+directory+" -userName "+webin_accout+" -password '"+suppasswd+"'  -validate")
+                    print('\n')
+                    os.system("java -jar modules/webin-cli-5.2.0.jar -context reads -manifest "+directory+"/manifest_run"+str(i-4)+".txt -inputDir "+directory+" -userName "+webin_accout+" -password '"+suppasswd+"'  -submit")
+                print('\n\n')
             else :
-                os.system('java -jar modules/webin-cli-5.2.0.jar -context reads -manifest '+directory+'/manifest_run'+str(i-4)+'.txt -inputDir '+directory+' -userName '+webin_accout+' -password '+suppasswd+'  -validate')
-                print('\n')
-                os.system('java -jar modules/webin-cli-5.2.0.jar -context reads -manifest '+directory+'/manifest_run'+str(i-4)+'.txt -inputDir '+directory+' -userName '+webin_accout+' -password '+suppasswd+'  -submit')
-            print('\n\n')
+                print('\033[91m'+str(i-4)+' _No Fastq File')
+                print('\033[0m')
             # os.system('rm '+directory+'/manifest_run.txt')    
     
         print('\033[92m'+'DONE...\n') 
@@ -182,34 +196,50 @@ class DDSubmission:
                     run_id = j[0] 
                 manifest.write('STUDY\t'+study_id+'\n') 
                 manifest.write('SAMPLE\t'+sample_id+'\n')
-                manifest.write('RUN_REF\t'+run_id+'\n')
+                if run_id != '':
+                    manifest.write('RUN_REF\t'+run_id+'\n')
                 sup_metadata = list(metadata.iloc[i])
                 for j in range(header_2.index('assemblyname'),header_2.index('fasta/flatfile name')+1):
                     if header_2[j]== 'run_ref' :
                         pass
-                    elif header_2[j]== 'fasta/flatfile name' :
-                        manifest.write('FASTA\t'+str(sup_metadata[j])+'\n')
-                    else :
+                    elif header_2[j]== 'fasta/flatfile name':
+                        fa = 1
+                        if sup_metadata[j] != -1 :
+                            manifest.write('FASTA\t'+str(sup_metadata[j])+'\n')
+                        else :
+                            fa = 0
+                    elif sup_metadata[j] != -1  :
                         manifest.write(header_2[j].upper()+'\t'+str(sup_metadata[j])+'\n')
                 manifest.write('CHROMOSOME_LIST\t'+str(sup_metadata[header_2.index('fasta/flatfile name')]).split('.fasta')[0]+'_chrm_list.txt.gz\n')
-                CHROMOSOME_LIST = open(directory+str(sup_metadata[header_2.index('assemblyname')])+'_chrm_list.txt','w')
-                CHROMOSOME_LIST.write(str(sup_metadata[header_2.index('assemblyname')]).replace('_','/')+'\t1\tMonopartite')
-                os.system('gzip '+directory+str(sup_metadata[header_2.index('assemblyname')])+'_chrm_list.txt')
+                CHROMOSOME_LIST = open(directory+'/'+str(sup_metadata[header_2.index('fasta/flatfile name')]).split('.fasta')[0]+'_chrm_list.txt','w')
+                CHROMOSOME_LIST.write(str(sup_metadata[header_2.index('assemblyname')])+'\t1\tMonopartite')
                 CHROMOSOME_LIST.close()
+                print('\033[92m'+'\nPress y then enter ...')
+                print ('\033[93m')
+                os.system('cd ' +directory+' ; gzip '+str(sup_metadata[header_2.index('fasta/flatfile name')]).split('.fasta')[0]+'_chrm_list.txt')
+                print('\033[0m')
                 manifest.close()
-                
-                if t == '-t':
-                    os.system('java -jar modules/webin-cli-5.2.0.jar -context genome -manifest '+directory+'/manifest_analysis'+str(i-4)+'.txt -inputDir '+directory+' -userName '+webin_accout+' -password '+suppasswd+' -test -validate')
-                    print('\n')
-                    os.system('java -jar modules/webin-cli-5.2.0.jar -context genome -manifest '+directory+'/manifest_analysis'+str(i-4)+'.txt -inputDir '+directory+' -userName '+webin_accout+' -password '+suppasswd+' -test -submit')
+                if fa != 0:
+                    if t == '-t':
+                        os.system("java -jar modules/webin-cli-5.2.0.jar -context genome -manifest "+directory+"/manifest_analysis"+str(i-4)+".txt -inputDir "+directory+" -userName '"+webin_accout+"' -password '"+suppasswd+"' -test -validate")
+                        print('\n')
+                        os.system("java -jar modules/webin-cli-5.2.0.jar -context genome -manifest "+directory+"/manifest_analysis"+str(i-4)+".txt -inputDir "+directory+" -userName '"+webin_accout+"' -password '"+suppasswd+"' -test -submit")
+                    else :
+                        os.system("java -jar modules/webin-cli-5.2.0.jar -context genome -manifest "+directory+"/manifest_analysis"+str(i-4)+".txt -inputDir "+directory+" -userName '"+webin_accout+"' -password '"+suppasswd+"' -validate")
+                        print('\n')
+                        os.system("java -jar modules/webin-cli-5.2.0.jar -context genome -manifest "+directory+"/manifest_analysis"+str(i-4)+".txt -inputDir "+directory+" -userName '"+webin_accout+"' -password '"+suppasswd+"' -submit")
+                    print('\n\n')
                 else :
-                    os.system('java -jar modules/webin-cli-5.2.0.jar -context genome -manifest '+directory+'/manifest_analysis'+str(i-4)+'.txt -inputDir '+directory+' -userName '+webin_accout+' -password '+suppasswd+'  -validate')
-                    print('\n')
-                    os.system('java -jar modules/webin-cli-5.2.0.jar -context genome -manifest '+directory+'/manifest_analysis'+str(i-4)+'.txt -inputDir '+directory+' -userName '+webin_accout+' -password '+suppasswd+'  -submit')
-                print('\n\n')
+                    print('\033[91m'+str(i-4)+' _No Fasta File')
+                    print('\033[0m')
                 # os.system('rm '+directory+'/manifest_analysis.txt')
             
         print('\033[92m'+'DONE...\n') 
+        end = datetime.now()
+        print('\033[92m'+'[ '+str(start)+' ] Start time ...')
+        print('\033[0m')
+        print('\033[92m'+'[ '+str(end)+' ] End time ...')
+        print('\033[0m')
         print('\033[0m')
         
         a.close()
